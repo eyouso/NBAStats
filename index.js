8f8808550bef3a734faa952f591fb81b1c2bbb17
Ethan <ethanyouso@gmail.com>
2024-02-07 11:38:29 -0600

    Get everything linked up

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://www.balldontlie.io/api/v1/";
 
+app.use(express.static("public"));
 
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
