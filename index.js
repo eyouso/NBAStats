09306de3e117c3dafcca4cfa7f531a653478bd77
Ethan <ethanyouso@gmail.com>
2024-02-26 11:48:51 -0600

    get random player names

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://www.balldontlie.io/api/v1";
+const API_URL = "https://api.balldontlie.io/v1";
 const API_KEY = "3ead9e65-a189-4b8c-9262-c31ffa2548db";
 const config = {
   headers: { Authorization: API_KEY },
@@ -14,17 +14,19 @@ const config = {
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function getRandomID() {
+    return Math.floor(Math.random() * 3092);
+}
+
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
-app.post("/get-players", async (req, res) => {
-    const searchId = req.body.id;
-    console.log(searchId);
+app.post("/get-player", async (req, res) => {
+    const id = getRandomID();
     try {
-        const result = await axios.get(API_URL + "/players/" + "?search=" + searchId, config);
-        res.render("index.ejs", { content: JSON.stringify(result.data) });
-        console.log(result.data);
+        const result = await axios.get(API_URL + "/players/" + id, config);
+        res.render("index.ejs", { content: result.data  });
     } catch (error) {
         res.render("index.ejs", { content: JSON.stringify(error.response.data) });
     }
